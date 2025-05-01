9dec68af03bdfdbdc3771d8e2750b87003ef5222
alexis andrew <83471846+alexis-martel@users.noreply.github.com>
2025-04-30 22:43:31 -0400

    Add protoype functionality

--- Phototropic_Solar_Panel.ino
+++ Phototropic_Solar_Panel.ino
@@ -7,27 +7,31 @@
   --------
   All angle measures are in radians!
   Panel must be powered on at position zero to prevent cable management vortex hell!
+  The `axle_angle` grows counterclockwise
 */
 
 #include <Stepper.h>            // Needed to control the motor through the TB6612 driver
 #include <Adafruit_INA260.h>    // Needed to get voltage, amperage and power measurements from the INA260 breakout via I^2C
 #include "SdFat.h"              // Needed for historical measurements of INA260 data
+#include <SPI.h>                // Needed for historical measurements of INA260 data
 #include <Adafruit_NeoPixel.h>  // Needed to control the on-board NeoPixel
+SdFat SD;
 
 // Preprocessor constants
 #define AXLE_MOTOR_STEPS 100
+#define AXLE_MOTOR_STEP_SIZE 1
 #define AXLE_MOTOR_SPEED 60
 #define NUM_NEOPIX 1
-#define PIN_PHOTORES_1 A0
-#define PIN_PHOTORES_2 A1
-
-// Global constants
-const double AXLE_GEAR_RATIO = 3 / 5;
+#define PIN_PHOTORES_1 A1
+#define PIN_PHOTORES_2 A2
+#define SD_FAT_TYPE 3
+#define SD_CS_PIN SS
 
 // Global variables
 double axle_angle = 0;
+int left_photores = 0;
+int right_photores = 0;
 
-Adafruit_NeoPixel neopixel(NUM_NEOPIX, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 Stepper axle_motor(AXLE_MOTOR_STEPS, 4, 5, 6, 7);
 
 // Helper functions
@@ -90,14 +94,35 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
   digitalWrite(LED_BUILTIN, LOW);
-  // Testing
-  while (!Serial) {
-    delay(1);
+  // TODO: Remove this statement for headless use
+  while (!Serial.available()) {
+    yield();
   }
-  Serial.print(reduceAngle((PI/4)+PI));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  axle_angle = reduceAngle(axle_angle);
+  left_photores = analogRead(PIN_PHOTORES_1);
+  right_photores = analogRead(PIN_PHOTORES_2);
+  Serial.print("Left: ");
+  Serial.print(left_photores);
+  Serial.print(", Right: ");
+  Serial.print(right_photores);
+  Serial.print(", Angle: ");
+  Serial.println(axle_angle);
+  // Check which photoresitor is higher
+  if (left_photores > right_photores) {
+    Serial.println("Moving counterclockwise");
+    axle_motor.step(-AXLE_MOTOR_STEP_SIZE);
+    axle_angle += (AXLE_MOTOR_STEP_SIZE / AXLE_MOTOR_STEPS) * TWO_PI;
+  } else if (right_photores > left_photores) {
+    Serial.println("Moving clockwise");
+    axle_motor.step(AXLE_MOTOR_STEP_SIZE);
+    axle_angle -= (AXLE_MOTOR_STEP_SIZE / AXLE_MOTOR_STEPS) * TWO_PI;
+  } else {
+    // Do nothing, the panel is perfectly centred
+    Serial.println("Standing by");
+  }
+  delay(300);
 }
